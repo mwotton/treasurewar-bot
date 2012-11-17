7afb12b1dabff63455fb23d9c44655b38dd18a79
Mark Wotton <mwotton@gmail.com>
2012-11-17 17:16:37 +1100

    refactorings

--- bot.rb
+++ bot.rb
@@ -1,60 +1,97 @@
 require 'SocketIO'
 require "curses"
 
+def all_points(left, right, top, bottom)
+  (top..bottom).each do |x|
+    (left..right).each do |y|
+      yield x,y
+    end
+  end
+end
+
+class DrunkenWalker
+  def choose(state)
+    return(['move', {dir: ['n', 'e', 's', 'w'].sample}])
+  end
+end
+
+class WallHugger
+  def choose(state)
+
+  end
+end
+
+
+def render_dashboard(state)
+  Curses.setpos(Curses.lines / 2, Curses.cols / 2)
+  Curses.addstr("P")
+  Curses.setpos(1, 1)
+  Curses.addstr("Health: #{state['you']['health']} Treasure: #{state['nearby_treasure'].inspect}")
+  Curses.setpos(2, 1)
+  Curses.addstr("Stashes: #{state['nearby_stashes'].inspect}")
+  Curses.setpos(3, 1)
+  Curses.addstr("Current Position: #{state['you']['position']['x']}, #{state['you']['position']['y']}")
+  Curses.setpos(4, 1)
+  Curses.addstr("Stash Position: #{state['you']['stash']['x']}, #{state['you']['stash']['y']}")
+  Curses.refresh
+end
+
+def update_world(new_tiles, you)
+  return_tiles = {}
+  sight_tiles = {}
+  new_tiles.each do |tile|
+    sight_tiles[[tile['x'], tile['y']]] = tile
+  end
+  
+  you_x = you['position']['x']
+  you_y = you['position']['y']
+  
+  all_points(-2,2,-2,2) do |x,y|
+    ix = [you_x + x, you_y + y]
+    return_tiles[ix] = sight_tiles[ix].nil? ? ' ' : '#'
+  end
+  return_tiles[[you['stash']['x'], you['stash']['y']]] = "S"
+  return return_tiles
+end
+
 tiles = {}
 auto_explore = true
+strategy = DrunkenWalker.new
+
 Curses::init_screen
 begin
   Curses::cbreak
   Curses::noecho
   Curses::timeout=(0)
+  puts strategy.inspect
+  
   client = SocketIO.connect("http://localhost:8000") do
     before_start do
       on_message {|message| puts "incoming message: #{message}"}
 
       # You have about 2 secs between each tick
-      on_event('tick') { |game_state|
+      on_event('tick') do |game_state|
+        
         state = game_state.first
-        tiles[[state['you']['stash']['x'], state['you']['stash']['y']]] = "S"
+        you = state['you']
+        tiles.merge!(update_world(state['tiles'], you))
+
+        you_x = you['position']['x']
+        you_y = you['position']['y']        
         
-        sight_tiles = {}
-        state['tiles'].each { |tile|
-          sight_tiles[[tile['x'], tile['y']]] = tile
-        }
-        (-2..2).each { |y| 
-          (-2..2).each { |x|
-            if (sight_tiles[[state['you']['position']['x'] + x, state['you']['position']['y'] + y]]== nil)
-              tiles[[state['you']['position']['x'] + x, state['you']['position']['y'] + y]] = " "
-            else
-              tiles[[state['you']['position']['x'] + x, state['you']['position']['y'] + y]] = "#"
-            end
-          }
-        }
-        
-        (-(Curses.lines / 2)..Curses.lines / 2).each { |yoffset|
-          (-(Curses.cols / 2)..Curses.cols / 2).each { |xoffset|
-            Curses.setpos((Curses.lines / 2) + yoffset, (Curses.cols / 2) + xoffset)
-            Curses.addstr(tiles[[state['you']['position']['x'] + xoffset, state['you']['position']['y'] + yoffset]] == nil ? "." : tiles[[state['you']['position']['x'] + xoffset, state['you']['position']['y'] + yoffset]])
-          }
-        }
-        
-        Curses.setpos(Curses.lines / 2, Curses.cols / 2)
-        Curses.addstr("P")
-        Curses.setpos(1, 1)
-        Curses.addstr("Health: #{state['you']['health']} Treasure: #{state['nearby_treasure'].inspect}")
-        Curses.setpos(2, 1)
-        Curses.addstr("Stashes: #{state['nearby_stashes'].inspect}")
-        Curses.setpos(3, 1)
-        Curses.addstr("Current Position: #{state['you']['position']['x']}, #{state['you']['position']['y']}")
-        Curses.setpos(4, 1)
-        Curses.addstr("Stash Position: #{state['you']['stash']['x']}, #{state['you']['stash']['y']}")
-        Curses.refresh
+        all_points((-Curses.lines)/2, Curses.lines/2,
+                   (-Curses.cols)/2, Curses.cols/2) do |xoffset,yoffset|
+          Curses.setpos((Curses.lines / 2) + yoffset, (Curses.cols / 2) + xoffset)
+          ix = [you_x + xoffset, you_y + yoffset]
+          Curses.addstr(tiles[ix] == nil ? "." : tiles[ix])
+        end
+
+        render_dashboard state
+
         if auto_explore
-          emit('move', {dir: ['n', 'e', 's', 'w'].sample})
+          emit(*strategy.choose(state))
           command = Curses.getch
-          if command == 'p'
-            auto_explore = false
-          end
+          auto_explore = false           if command == 'p'
         else
           direction = Curses.getch
           if ['n', 'e', 's', 'w'].include? direction
@@ -63,7 +100,7 @@ begin
             auto_explore = true
           end
         end
-      }
+      end
     end
 
     after_start do
