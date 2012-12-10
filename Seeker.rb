1ea513ee4d5b9aefb55a07685bbc5f47d07ce312
Mark Wotton <mwotton@gmail.com>
2012-12-10 17:33:41 +1100

    null out cached path once we finish it

--- Seeker.rb
+++ Seeker.rb
@@ -35,16 +35,21 @@ class Seeker < Bot
     # possible that the @cachedpath is old - check it's reasonable.
     if @cachedpath && !@cachedpath.empty?
       if BotUtils.to_dir(you['position'], @cachedpath.first) == nil
-        $stderr.puts "Can't follow #{@cachedpath} from #{you['position']} - resetting"
+        $stderr.puts "#{self.class} can't follow #{@cachedpath} from #{you['position']} - resetting"
         @cachedpath = nil
       end
     end
     @cachedpath ||= getpath(state,tiles)
     
+    $stderr.puts({ cachedpath: @cachedpath}.inspect)
     if @cachedpath && !@cachedpath.empty?
       best = BotUtils.to_dir(you['position'], @cachedpath.shift)
-      $stderr.puts("#{self.class} going for #{best}")
-      return ['move', { "dir" => best } ]       if best
+      @cachedpath = nil if @cachedpath.empty?
+      if best
+        $stderr.puts("#{self.class} going for #{best}")
+        return ['move', { "dir" => best } ]
+      end
+ 
     end
     nil
   end
